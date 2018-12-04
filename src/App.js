8f56f16aa32d3b659a39dff2bd91a4e9d01aeed1
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 12:23:31 +0000

    Initial creation of search widget

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchWidget from './Components/SearchWidget';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <SearchWidget />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/SearchWidget.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchWidget extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default SearchWidget;

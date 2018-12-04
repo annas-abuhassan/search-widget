fa1e15832b7abe6f86d3099027c35194b6a05801
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 16:24:40 +0000

    refactored the parsing of result type to reduce CSS

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -5,8 +5,11 @@ const Result = ({ result }) => {
   const { bookingId, name, country, region } = result;
   return bookingId ? (
     <div className="result">
-      <div className={`result-type-${parseResultType(bookingId)}`}>
-        {parseResultType(bookingId)}
+      <div
+        className="result-type"
+        style={{ 'background-color': parseResultType(bookingId)[1] }}
+      >
+        {parseResultType(bookingId)[0]}
       </div>
       <div className="result-body">
         <div className="result-name">{name} </div>
@@ -21,10 +24,21 @@ const Result = ({ result }) => {
 };
 
 const parseResultType = bookingId => {
+  const resultRefObj = {
+    city: 'aqua',
+    train: 'grey',
+    airport: 'salmon',
+    district: 'green',
+    region: 'yellow'
+  };
+
   const resultType = bookingId.slice(0, bookingId.indexOf('-'));
   return resultType === 'train'
-    ? 'Station'
-    : resultType.charAt(0).toUpperCase() + resultType.slice(1);
+    ? ['Station', 'grey']
+    : [
+        resultType.charAt(0).toUpperCase() + resultType.slice(1),
+        resultRefObj[resultType]
+      ];
 };
 
 export default Result;

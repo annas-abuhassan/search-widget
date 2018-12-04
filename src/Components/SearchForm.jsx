c1c270f7e0341add42c3ba2f69ecded1954fb3e0
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 12:42:39 +0000

    state updates with input value

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import './SearchForm.css';
 
 class SearchForm extends Component {
+  state = {
+    term: ''
+  };
   render() {
-    return <div />;
+    return (
+      <div>
+        <input
+          onChange={this.onChange}
+          placeholder="city, airport, station, region, district..."
+          className="input-field"
+        />
+      </div>
+    );
   }
+
+  onChange = event => {
+    const newTerm = event.target.value;
+    this.setState({
+      term: newTerm
+    });
+  };
 }
 
 export default SearchForm;

00e51d0f44b1a2ed7b73ac29285a0a3bd0eb69cb
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 16:08:50 +0000

    Modified required number of characters before search

--- src/api.js
+++ src/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const getSearchResults = term => {
   const API_URL = `https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${term}`;
 
-  if (term.length >= 3)
+  if (term.length >= 2)
     return axios.get(API_URL).then(({ data }) => data.results.docs);
 };

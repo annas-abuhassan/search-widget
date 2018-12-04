ee417370ab582c88605b64ab549c05ea9336bcf3
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 13:15:11 +0000

    api.js contains all functions that handle api calls

---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const getSearchResults = term => {
+  const API_URL = `https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${term}`;
+
+  if (term.length >= 3)
+    return axios.get(API_URL).then(({ data }) => data.results.docs);
+};

5f25a7d8b72c68ccb01188fca79c4294f302ad7e
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2018-12-04 12:57:38 +0000

    brought search form into search widget

--- src/Components/SearchWidget.jsx
+++ src/Components/SearchWidget.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import SearchForm from './SearchForm';
+import './SearchWidget.css';
 
 class SearchWidget extends Component {
   render() {
-    return <div />;
+    return (
+      <div className="search-widget-container">
+        <h1>Where are you going?</h1>
+        <div>Pick-up Location</div>
+        <SearchForm />
+      </div>
+    );
   }
 }
 
